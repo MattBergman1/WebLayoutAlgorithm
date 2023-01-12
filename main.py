1048d244c73ceb85bf25353c18b76c8c5ae18a53
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-01-12 10:32:22 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+
+chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+randomChar = random.choice(chars)
+
+with open('nonsense.txt', 'a') as f:
+    f.write(randomChar)
